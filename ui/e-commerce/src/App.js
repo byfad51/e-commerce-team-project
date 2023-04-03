0b3c7de2741d28725b0f587b0ae390820262ac92
mehmet-can-yakit <109636142+mehmet-can-yakit@users.noreply.github.com>
2023-04-04 00:26:11 +0300

    login

--- ui/e-commerce/src/App.js
+++ ui/e-commerce/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Login, Register } from './components/auth.js';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+            </li>
+            <li>
+         
+            </li>
+          </ul>
+        </nav>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+          
+      </Routes>
+    </BrowserRouter>
+      </div>
+   
   );
-}
+};
 
 export default App;
