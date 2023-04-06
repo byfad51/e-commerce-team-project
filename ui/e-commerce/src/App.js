8872397892081a5a69d59781185b1d289bd6e182
byfad51 <byfad51@gmail.com>
2023-04-07 00:51:26 +0300

    new design and some fixs for auth

--- ui/e-commerce/src/App.js
+++ ui/e-commerce/src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import PasswordForgot from "./components/PasswordForgot";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+          <Route path="/passwordforgot" element={<PasswordForgot />} />
         <Route  path="/register" element={<Register />}>
         </Route>
+
       </Routes>
     </Router>
   );
