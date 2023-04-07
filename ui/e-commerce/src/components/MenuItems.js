cd914f732a4a0cc4d0f314ec8e382369835967b2
esmnur <esmanurebes38@gmail.com>
2023-04-07 20:38:03 +0300

    navbar added

---
+++ ui/e-commerce/src/components/MenuItems.js
@@ -0,0 +1,23 @@
+
+export const MenuItems=[
+    {
+        title:'Home',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Products',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Login',
+        url:'./Login',
+        cName: 'nav-links'
+    },
+    {
+        title:'-',
+        url:'#',
+        cName: 'nav-links-mobile'
+    }
+]
