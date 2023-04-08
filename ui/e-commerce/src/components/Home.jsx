66b0f3e748680bd49be40127849c2c3fc200e881
esmnur <esmanurebes38@gmail.com>
2023-04-08 18:18:47 +0300

    navbar and sidebar

--- ui/e-commerce/src/components/Home.jsx
+++ ui/e-commerce/src/components/Home.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import {Carousel, Button, Container} from 'react-bootstrap';
 import Navbar from "./Navbar";
+import Mya from './Mya';
 
 function Home() {
   const navigate = useNavigate();
 
 
   return(
-  <Navbar />
-
+    <div>
+      <Navbar />
+      <div style={{ display: 'flex' }}>
+        <Mya />
+        <div style={{ flexGrow: 1 }}>
+        </div>
+      </div>
+    </div>
     
   );
   }
